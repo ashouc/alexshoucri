26733b02a7e579d4de7c1345cb75aaf1b315f507
Alexandra Shoucri <alexshoucri@gmail.com>
2017-06-16 10:49:01 -0400

    update files to include latest easter egg

--- script.js
+++ script.js
@@ -4,48 +4,96 @@ var count = 0;
 
 function start(){
 	postInfo();
-	azizLight();	
+	setupPanel('div.shadow.outside:nth-child(1)');
+	setupPanel('div.shadow.outside:nth-child(2)');
+	setupPanel('div.shadow.outside:nth-child(3)');
+	setupPanel('div.shadow.outside:nth-child(4)');
+	setupRotation('.outside');
+	setupRotation('.circle');
+	rotate();	
 }
+
 
 function postInfo() {
 	setTimeout(function(){
 		$('.bio-box .name').css('opacity','1');
-	}, 2000);
-	setTimeout(function(){
 		$('.short-bio').css('opacity','1');
+		$('.contact').css('opacity','1');
 	}, 3000);
 	setTimeout(function(){
-		$('.icon-box').css('opacity','1');
+		$('.box').css('opacity','1');
 	}, 4000);
 }
 
-function azizLight(){
-	$('span.rotate i').on('click',function(){
-		count++;
-		if(count === 5) {
-			$('.container').css('display','none');
-			$('body').append(`<div class="aziz" style="width:100%;height:0;
-				padding-bottom: 46%;position:relative;"><iframe src="
-				https://giphy.com/embed/uIGHPjEfdc0Ni" width="100%" 
-				height="100%" style="position:absolute;top:0;" frameBorder="0" 
-				class="giphy-embed" allowFullScreen></iframe></div><div class="
-				close"><i class="fa fa-times gradient" aria-hidden="true"></i></div>`);
-			// count = 0;
+
+function setupPanel(target){
+	$(target).on('mousedown',function() {
+		$(this).removeClass('shadow');	
+	})
+	$(target).on('mouseup',function() {
+		$(this).addClass('shadow');
+	})
+}
+
+function setupRotation(target){
+	$(target).on('mousedown',function() {
+		$('div.rotate').addClass('turn');	
+	})
+	$(target).on('mouseup',function() {
+		$('div.rotate').removeClass('turn');
+	})
+}
+
+function rotate(){
+	$('div.outside').on('click', function(event){
+		if(event.target.id == "one" && event.target.attributes[1].value == "true" || event.target.id == "three" && event.target.attributes[1].value == "true") {
+			$id = $('#' + event.target.id);
+			$id.attr('indicator','false');
+			count++;
+			return;
+		}
+		else if(event.target.id == "two" && event.target.attributes[1].value == "true" || event.target.id == "four" && event.target.attributes[1].value == "true") {
+			$id = $('#' + event.target.id);
+			$id.attr('indicator','false');
+			count++;
+			return;
+		}
+		return;
+	});
+	$('div.circle').on('click', function(event){
+		if(event.target.offsetParent.id == "five" &&  count === 4) {
+			$id = $('#' + event.target.offsetParent.id);
+			$id.attr('indicator','false');
+			azizLight();
 			reset();
 			return;
 		}
 	});
 }
 
+function azizLight(){
+	$('.container').css('display','none');
+	$('body').append(`<div class="aziz" style="width:100%;height:0;
+		padding-bottom: 46%;position:relative;"><iframe src="
+		https://giphy.com/embed/uIGHPjEfdc0Ni" width="100%" 
+		height="100%" style="position:absolute;top:0;" frameBorder="0" 
+		class="giphy-embed" allowFullScreen></iframe></div><div class="
+		close"><i class="fa fa-times gradient" aria-hidden="true"></i></div>`);
+	reset();
+	return;
+}
+
 function reset(){
-	$('i.fa.fa-times').one('click',function(event){
+	$('i.fa.fa-times').one('click',function(){
 		count = 0;
-		console.log(event.target);
-		// $('.short-bio').css('display','block');
-		// $('.bio-box').css('padding','0 0 150px');
+		$('.container').css('display','block');
 		$('div.aziz').remove();
 		$('div.close').remove();
-		$('.container').css('display','block');
+		$('#one').attr('indicator','true');
+		$('#two').attr('indicator','true');
+		$('#three').attr('indicator','true');
+		$('#four').attr('indicator','true');
+		$('#five').attr('indicator','true');			
 	});
 }
 
